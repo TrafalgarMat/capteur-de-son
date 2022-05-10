e9ac9a8d72756e6b735fb2d2aea4b453ec9fb718
TrafalgarMat <103508401+TrafalgarMat@users.noreply.github.com>
2022-05-10 09:46:14 +0200

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
 basic.forever(function () {
-    if (input.soundLevel() > 100) {
-        basic.showIcon(IconNames.Heart)
-    } else {
-        basic.showIcon(IconNames.SmallHeart)
-    }
+    led.plotBarGraph(
+    input.soundLevel(),
+    255
+    )
 })
